30c9f34c2b3e41f4b67f608217e63d66ee5aa0ca
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2023-10-30 14:50:27 -0300

    Exemplos Iniciais de SOCKETS

---
+++ 2023-10-23 - SOCKETS/exemplo_03.py
@@ -0,0 +1,16 @@
+import socket
+
+server = input('\nInforme o nome do HOST ou URL do site: ')
+
+infos = socket.getaddrinfo(host=server, port=80)
+
+print(infos)
+
+for info in infos:
+   print('\n----------------------------------------')
+   print(f'Info ...................: {info}')
+   print(f'Family .................: {info[0]}')
+   print(f'Type ...................: {info[1]}')
+   print(f'Proto ..................: {info[2]}')
+   print(f'Canonical Name (CNAME) .: {info[3]}')   
+   print(f'SOCKET Address .........: {info[4]}')
---
+++ 2023-10-23 - SOCKETS/exemplo_04.py
@@ -0,0 +1,29 @@
+import socket, sys
+
+#------------------------------------------------------------
+def get_protnumber(prefix):
+   return dict ((getattr(socket, a), a)
+      for a in dir(socket)
+         if a.startswith(prefix) )
+#------------------------------------------------------------
+
+proto_fam = get_protnumber('AF_')
+types     = get_protnumber('SOCK_')
+protocols = get_protnumber('IPPROTO_')
+
+server = input('\nInforme o nome do HOST ou URL do site: ')
+
+infos = socket.getaddrinfo(host=server, port='http')
+
+print(infos)
+
+for info in infos:
+   family, socktype, proto, canonname, sockaddr = info
+   
+   print('\n----------------------------------------')
+   print(f'Info ...................: {info}')
+   print(f'Family .................: {proto_fam[family]}')
+   print(f'Type ...................: {types[socktype]}')
+   print(f'Proto ..................: {protocols[proto]}')
+   print(f'Canonical Name (CNAME) .: {canonname}')   
+   print(f'SOCKET Address .........: {sockaddr}')
